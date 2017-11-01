9df9ceb02454e0aac622f9ca004a40720d9bb2db
李蒙 <limeng@novacloud.com>
2017-11-01 11:20:20 +0800

    init

---
+++ ios/RCTNotify/RCTNotify.h
@@ -0,0 +1,13 @@
+//
+//  RCTNotify.h
+//  RCTNotify
+//
+//  Created by NovaCloud on 2017/11/1.
+//  Copyright © 2017年 FreshPort. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTNotify : NSObject
+
+@end
---
+++ ios/RCTNotify/RCTNotify.m
@@ -0,0 +1,13 @@
+//
+//  RCTNotify.m
+//  RCTNotify
+//
+//  Created by NovaCloud on 2017/11/1.
+//  Copyright © 2017年 FreshPort. All rights reserved.
+//
+
+#import "RCTNotify.h"
+
+@implementation RCTNotify
+
+@end
